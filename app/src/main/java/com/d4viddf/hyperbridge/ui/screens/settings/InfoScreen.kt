686ca6286f58810456b3c4f8c5408f4fdb68c1ae
david <dominguezfondodavid@gmail.com>
2026-09-13 00:42:08 +0200

    feat(info): acknowledge Sykeptical for screen recording, messaging, calls and vpn contributions

--- app/src/main/java/com/d4viddf/hyperbridge/ui/screens/settings/InfoScreen.kt
+++ app/src/main/java/com/d4viddf/hyperbridge/ui/screens/settings/InfoScreen.kt
@@ -244,6 +244,13 @@ fun InfoScreen(
                         stringResource(R.string.xmsf_workaround_credit_subtitle)
                     ) {
                         uriHandler.openUri("https://www.coolapk1s.com/feed/70418983")
+                    },
+                    SettingsItemData(
+                        Icons.Default.Favorite,
+                        stringResource(R.string.sykeptical_credit_title),
+                        stringResource(R.string.sykeptical_credit_subtitle)
+                    ) {
+                        uriHandler.openUri("https://github.com/SykepticalS/Sykepticals_HyperBridge")
                     }
                 )
             )
